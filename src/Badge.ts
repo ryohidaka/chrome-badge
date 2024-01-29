269f6ab452e03408736a327bb20672d1b9ce701d
ryohidaka <39184410+ryohidaka@users.noreply.github.com>
2024-01-29 19:31:17 +0900

    feat: Add function to wrap getBadge.

--- src/Badge.ts
+++ src/Badge.ts
@@ -1,4 +1,4 @@
-import { BadgeProps } from "./types";
+import { BadgeProps, BadgeStatus } from "./types";
 
 /**
  * Badge object for managing badge text, color and background color.
@@ -25,6 +25,25 @@ export const Badge = {
       });
 
       callback?.();
+    } catch (error) {
+      console.error(error);
+    }
+  },
+
+  /**
+   * Gets the badge text, text color, and background color.
+   * @param {number} tabId - The ID of the tab.
+   * @returns {Promise<BadgeStatus | undefined>}
+   */
+  get: async (tabId?: number): Promise<BadgeStatus | undefined> => {
+    try {
+      const text = await chrome.action.getBadgeText({ tabId });
+      const textColor = await chrome.action.getBadgeTextColor({ tabId });
+      const backgroundColor = await chrome.action.getBadgeBackgroundColor({
+        tabId,
+      });
+
+      return { text, textColor, backgroundColor };
     } catch (error) {
       console.error(error);
     }
--- src/types.ts
+++ src/types.ts
@@ -10,6 +10,15 @@ export type BadgeProps = {
 };
 
 /**
+ * Status of the badge.
+ */
+export type BadgeStatus = {
+  text: string; // The text displayed on the badge.
+  textColor?: Color; // The color of the badge text.
+  backgroundColor?: Color; // The background color of the badge.
+};
+
+/**
  * Type for the color details of the badge.
  */
 type Color = chrome.action.BadgeColorDetails["color"];
